4186432e35d95bb49a09b6e89fc1cc81a9e8f3da
Rangi <remy.oukaour+rangi42@gmail.com>
2020-02-05 22:12:08 -0500

    Remove most GSC maps, but not landmarks
    Search for "TODO" to see what needs editing as you add your own maps.

--- data/radio/oaks_pkmn_talk_routes.asm
+++ data/radio/oaks_pkmn_talk_routes.asm
@@ -2,18 +2,19 @@
 
 OaksPKMNTalkRoutes:
 ; there are NUM_OAKS_POKEMON_TALK_ROUTES entries
-	map_id ROUTE_29
-	map_id ROUTE_46
-	map_id ROUTE_30
-	map_id ROUTE_32
-	map_id ROUTE_34
-	map_id ROUTE_35
-	map_id ROUTE_37
-	map_id ROUTE_38
-	map_id ROUTE_39
-	map_id ROUTE_42
-	map_id ROUTE_43
-	map_id ROUTE_44
-	map_id ROUTE_45
-	map_id ROUTE_36
-	map_id ROUTE_31
+; TODO: Choose 15 maps to use here.
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
+	map_id NONE
